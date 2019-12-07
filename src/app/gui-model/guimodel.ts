dbefe81c0a1046558eef51ba06b88a3b9d6b5c10
dietltho <dietltho@students.zhaw.ch>
2019-12-07 12:19:06 +0100

    formlist add for groups

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -248,6 +248,16 @@ export class GuiModel {
                             "form": {
                                 "form": "GroupForm"
                             }
+                        },
+                        {
+                            "type": "list",
+                            "icon": "fa-weixin",
+                            "color": "wisteria",
+                            "search": true,
+                            "url": "/group",
+                            "form": {
+                                "form": "GroupForm"
+                            }
                         }
                     ]
                 }
